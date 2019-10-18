25afd75c94514d583cb6b8d5a877ecb83286957f
Krisztian Holub <kholub1989@gmail.com>
2019-10-17 20:17:43 -0400

    Creating API

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express')
 var app = express();
 var mongoose = require('mongoose');
 var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 
 var port = process.env.PORT || 3000;
 
@@ -14,7 +15,8 @@ app.set('view engine', 'ejs');
 mongoose.connect('mongodb+srv://' + process.env.uname + ':' + process.env.pwd + '@cluster0-tkdzb.mongodb.net/test?retryWrites=true&w=majority', { 
   useNewUrlParser: true,
   useUnifiedTopology: true });
-  
+
 setupController(app);
+apiController(app);
 
 app.listen(port);
