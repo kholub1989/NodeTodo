6c74c6ffcba91c933bd4e991be5497dd0825782d
Krisztian Holub <kholub1989@gmail.com>
2019-10-18 13:24:53 -0400

    add Front-End w Angular 2

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var apiController = require('./controllers/apiController');
 
 var port = process.env.PORT || 3000;
 
-app.use('/assets', express.static(__dirname, + '/public'));
+app.use('/', express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
